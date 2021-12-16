9632978ec402c2a2b634012b36f0a4e8879c4426
Geoffrey Beausire <g.beausire@criteo.com>
2021-12-16 17:44:20 +0100

    Add per namespace check

--- probes/aerospike/endpoint.go
+++ probes/aerospike/endpoint.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
+	"log"
+	"regexp"
 
 	as "github.com/aerospike/aerospike-client-go"
 )
 
+var (
+	setExtractionRegex, _ = regexp.Compile("ns=([a-zA-Z0-9]+):")
+)
+
 type AerospikeEndpoint struct {
-	Name   string
-	Client *as.Client
-	Config AerospikeClientConfig
+	Name       string
+	Client     *as.Client
+	Config     AerospikeClientConfig
+	namespaces map[string]struct{}
 }
 
-func (e AerospikeEndpoint) Hash() string {
+func (e *AerospikeEndpoint) Hash() string {
 	return e.Name
 }
 
-func (e AerospikeEndpoint) GetName() string {
+func (e *AerospikeEndpoint) GetName() string {
 	return e.Name
 }
 
-func (e AerospikeEndpoint) Connect() error {
+func (e *AerospikeEndpoint) Connect() error {
 	clientPolicy := as.NewClientPolicy()
 
 	if e.Config.tlsEnabled {
@@ -48,9 +56,32 @@ func (e AerospikeEndpoint) Connect() error {
 		return err
 	}
 	e.Client = client
+	e.Refresh()
 	return nil
 }
 
-func (e AerospikeEndpoint) Close() error {
+func (e *AerospikeEndpoint) Refresh() error {
+	nodes := e.Client.GetNodes()
+
+	infop := as.NewInfoPolicy()
+	e.namespaces = make(map[string]struct{})
+	for _, n := range nodes {
+
+		data, err := n.RequestInfo(infop, fmt.Sprintln("sets"))
+		if err != nil {
+			return err
+		}
+		for _, val := range data {
+			matches := setExtractionRegex.FindStringSubmatch(val)
+			if len(matches) > 1 {
+				e.namespaces[matches[1]] = struct{}{}
+			}
+		}
+	}
+	log.Println("Refresh finished: current namespaces: ", e.namespaces)
+	return nil
+}
+
+func (e *AerospikeEndpoint) Close() error {
 	return nil
 }
